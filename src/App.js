feaa1ee64352bcf6b40f1cc78d653988f0164105
Szymekk <skucharek2@gmail.com>
2022-06-11 22:51:34 +0200

    Base app working with API, showing non formatted data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React from 'react';
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    users: [],
+    error: null
+  };
+  getFetchUsers() {
+    this.setState({
+      loading: true
+    }, () => {
+      fetch("http://localhost:3001/json").then(res => res.json()).then(result => this.setState({
+        loading: false,
+        users: result
+      })).catch(console.log);
+    });
+  }
+  componentDidMount() {
+    this.getFetchUsers();
+  }
+  render() {
+    const {
+      users,
+      error
+    } = this.state;
+    return (
+      <React.Fragment>
+        <h1>All Printers</h1>
+        {
+          error ? <p>
+            {
+              error.message
+            } < /p> : null}  {
+              users.map(user => {
+                const {
+                  ID,
+                  Producent,
+                  Model,
+                  Type,
+                  Desc,
+                  Price,
+                  Image
+                } = user;
+                return (
+                  <div id={ID} key={ID}>
+                    <img src={Image}/>
+                    <p>Name: {Model}</p>
+                    <p>Productent: {Producent}</p>
+                    <p>Type: {Type}</p>
+                    <p>Price: {Price}</p>
+                    <hr />
+                  </div>
+                );
+              })
+            } < /React.Fragment> );
+          }
+      }
+            export default App;
