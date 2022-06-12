c137add1974d2b94efebb509e32afb908bd1fb18
Szymekk <skucharek2@gmail.com>
2022-06-12 22:37:22 +0200

    naprawione nadmierne usuwanie z listy

--- src/App.js
+++ src/App.js
@@ -1,132 +1,159 @@
-import React from 'react';
+import React from "react";
 class App extends React.Component {
   state = {
     isLoading: true,
+    printersall: [],
     printers: [],
     typess: [],
     producents: [],
-    error: null
+    error: null,
   };
-  onChange = e => {
-    let temp = []
-    
+  onChange = (e) => {
+    console.log(this.state.printers.length)
+    let temp = [];
+      this.state.printers = this.state.printersall;
+
     switch (e.currentTarget.id) {
       case "Price":
         if (e.currentTarget.value === "ASC") {
           this.setState({
-            printers: this.state.printers.sort((a, b) => parseFloat(a.Price) - parseFloat(b.Price))
+            printers: this.state.printers.sort(
+              (a, b) => parseFloat(a.Price) - parseFloat(b.Price)
+            ),
           });
-        }
-        else if (e.currentTarget.value === "DESC") {
+        } else if (e.currentTarget.value === "DESC") {
           this.setState({
-            printers: this.state.printers.sort((a, b) => parseFloat(b.Price) - parseFloat(a.Price))
+            printers: this.state.printers.sort(
+              (a, b) => parseFloat(b.Price) - parseFloat(a.Price)
+            ),
           });
         }
         break;
       case "Type":
         for (let i = 0; i < this.state.printers.length; i++) {
-          if(this.state.printers[i].Type === e.currentTarget.value)
-            temp.push(this.state.printers[i])
+          if (this.state.printers[i].Type === e.currentTarget.value)
+            temp.push(this.state.printers[i]);
         }
         this.setState({
-          printers: temp
-        })
+          printers: temp,
+        });
 
         break;
-        default:
-          break;
+      default:
+        break;
     }
-
-
-  }
+  };
 
   getFetchprinters() {
-    this.setState({
-      loading: true
-    }, () => {
-      fetch("http://localhost:3001/json/").then(res => res.json()).then(result => this.setState({
-        loading: false,
-        printers: result //.sort((a, b) => parseFloat(a.Price) - parseFloat(b.Price))x
-      })).catch(console.log);
-      fetch("http://localhost:3001/json/types").then(res => res.json()).then(result => this.setState({
-        loading: false,
-        typess: result
-      })).catch(console.log);
-      fetch("http://localhost:3001/json/producents").then(res => res.json()).then(result => this.setState({
-        loading: false,
-        producents: result
-      })).catch(console.log);
-    });
+    this.setState(
+      {
+        loading: true,
+      },
+      () => {
+        fetch("http://localhost:3001/json/")
+          .then((res) => res.json())
+          .then((result) =>
+            this.setState({
+              loading: false,
+              printers: result,
+              printersall: result, //.sort((a, b) => parseFloat(a.Price) - parseFloat(b.Price))x
+            })
+          )
+          .catch(console.log);
+        fetch("http://localhost:3001/json/types")
+          .then((res) => res.json())
+          .then((result) =>
+            this.setState({
+              loading: false,
+              typess: result,
+            })
+          )
+          .catch(console.log);
+        fetch("http://localhost:3001/json/producents")
+          .then((res) => res.json())
+          .then((result) =>
+            this.setState({
+              loading: false,
+              producents: result,
+            })
+          )
+          .catch(console.log);
+      }
+    );
   }
-
-
 
   componentDidMount() {
     this.getFetchprinters();
   }
   render() {
-    const {
-      printers,
-      typess,
-      producents,
-      error
-    } = this.state;
+    const { printers, typess, producents, error } = this.state;
     return (
       <React.Fragment>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" />
-        <h1>All Printers</h1>
-        <div className="row row-cols-5">
-          <div>
-            <h1>Sort by:</h1>
-          </div>
-          <div>
-            <span>Price</span>
-            <select onChange={this.onChange} defaultValue="Select" id="Price" className="form-select">
-              <option value="ASC">ASC</option>
-              <option value="DESC">DESC</option>
-            </select>
-          </div>
-          <div>
-            <span>Producent</span>
-            <select onChange={this.onChange} defaultValue="Select" id="Producent" className="form-select">
-              {
-                error ? <p> {error.message} </p> : null}  {
-                producents.map(typ => {
+        <div>
+          <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+          />
+          <h1>All Printers</h1>
+          <div className="row row-cols-5">
+            <div>
+              <h1>Sort by:</h1>
+            </div>
+            <div>
+              <span>Price</span>
+              <select
+                onChange={this.onChange}
+                defaultValue="Select"
+                id="Price"
+                className="form-select"
+              >
+                <option value="ASC">ASC</option>
+                <option value="DESC">DESC</option>
+              </select>
+            </div>
+            <div>
+              <span>Producent</span>
+              <select
+                onChange={this.onChange}
+                defaultValue="Select"
+                id="Producent"
+                className="form-select"
+              >
+                {error ? <p> {error.message} </p> : null}{" "}
+                {producents.map((typ) => {
                   const { Producent } = typ;
-                  return (
-                    <option key={Producent}>{Producent}</option>
-                  );
-                })
-              }            </select>
-          </div>
-          <div>
-            <span>Type</span>
-            <select onChange={this.onChange} defaultValue="Select" id="Type" className="form-select">
-              {
-                error ? <p> {error.message} </p> : null}  {
-                typess.map(typ => {
+                  return <option key={Producent}>{Producent}</option>;
+                })}{" "}
+              </select>
+            </div>
+            <div>
+              <span>Type</span>
+              <select
+                onChange={this.onChange}
+                defaultValue="Select"
+                id="Type"
+                className="form-select"
+              >
+                {error ? <p> {error.message} </p> : null}{" "}
+                {typess.map((typ) => {
                   const { Type } = typ;
-                  return (
-                    <option key={Type}>{Type}</option>
-                  );
-                })
-              }
-            </select>
+                  return <option key={Type}>{Type}</option>;
+                })}
+              </select>
+            </div>
+            <div>
+              <button
+                class="btn btn-outline-dark"
+                onClick={this.getFetchprinters}
+              >
+                Reset
+              </button>
+            </div>
           </div>
-        </div>
-        {
-          error ? <p> {error.message} </p> : null}  {
-          printers.map(printer => {
-            const {
-              ID,
-              Producent,
-              Model,
-              Type,
-              Price,
-              Image
-            } = printer;
+          {error ? <p> {error.message} </p> : null}{" "}
+          {printers.map((printer) => {
+            const { ID, Producent, Model, Type, Price, Image } = printer;
             return (
-
               <div id={ID} key={ID}>
                 <img alt={Model} src={Image} />
                 <p>Name: {Model}</p>
@@ -136,8 +163,8 @@ class App extends React.Component {
                 <hr />
               </div>
             );
-          })
-        }
+          })}
+        </div>
       </React.Fragment>
     );
   }
